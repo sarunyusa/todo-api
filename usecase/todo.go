0553ff6e5370bac4160be87ac883c46ea1a5ff0e
Sarunyu Sakornmaneesup <sarunyu.s@inception.asia>
2020-06-25 22:08:58 +0700

    add usecase method

--- usecase/todo.go
+++ usecase/todo.go
@@ -1,16 +1,49 @@
 package usecase
 
 import (
+	"context"
 	"github.com/jinzhu/gorm"
+	"todo/pkg/model"
 	"todo/repository"
 )
 
 type TodoUseCase interface {
+	CreateTodo(ctx context.Context, content *model.TodoContent) (*model.TodoInfo, error)
+	UpdateTodo(ctx context.Context, content *model.TodoContent) (*model.TodoInfo, error)
+	DeleteTodo(ctx context.Context, id string) error
+	SetTodoDone(ctx context.Context, id string) error
+
+	GetNotDoneTodo(ctx context.Context) (*[]model.TodoInfo, error)
+	GetTodoById(ctx context.Context) (*model.TodoInfo, error)
 }
 
 type todoUseCase struct {
 	db       *gorm.DB
 	todoRepo repository.TodoRepository
+}
+
+func (t todoUseCase) CreateTodo(ctx context.Context, content *model.TodoContent) (*model.TodoInfo, error) {
+	panic("implement me")
+}
+
+func (t todoUseCase) UpdateTodo(ctx context.Context, content *model.TodoContent) (*model.TodoInfo, error) {
+	panic("implement me")
+}
+
+func (t todoUseCase) DeleteTodo(ctx context.Context, id string) error {
+	panic("implement me")
+}
+
+func (t todoUseCase) SetTodoDone(ctx context.Context, id string) error {
+	panic("implement me")
+}
+
+func (t todoUseCase) GetNotDoneTodo(ctx context.Context) (*[]model.TodoInfo, error) {
+	panic("implement me")
+}
+
+func (t todoUseCase) GetTodoById(ctx context.Context) (*model.TodoInfo, error) {
+	panic("implement me")
 }
 
 func NewTodoUseCase(db *gorm.DB, todoRepo repository.TodoRepository) TodoUseCase {
