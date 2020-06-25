1bb9a4ed55077b387640e0d27d2d5f78821c2746
Sarunyu Sakornmaneesup <sarunyu.s@inception.asia>
2020-06-26 01:52:15 +0700

    add UpdateTodo test case

--- usecase/todo_test.go
+++ usecase/todo_test.go
@@ -105,3 +105,84 @@ func TestTodoUseCase_CreateTodo(t *testing.T) {
 		assert.Equal(t, err.Error(), "db error")
 	})
 }
+
+func TestTodoUseCase_UpdateTodo(t *testing.T) {
+	t.Run("update success", func(t *testing.T) {
+		db, smock, repo := createMock()
+
+		todo := &entity.Todo{
+			Base: entity.Base{
+				ID:        util.NewID(),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				DeletedAt: nil,
+			},
+			Topic:   "test topic",
+			Detail:  ptr.String("test detail"),
+			DueDate: ptr.Time(time.Now()),
+			IsDone:  false,
+		}
+
+		repo.On("UpdateTodo", mock.Anything, mock.Anything, mock.Anything).
+			Return(todo, nil)
+
+		smock.ExpectBegin()
+		smock.ExpectCommit()
+
+		u := NewTodoUseCase(db, &repo)
+
+		res, err := u.UpdateTodo(context.Background(), &model.TodoContent{Topic: "test topic"})
+
+		require.Nil(t, err)
+		require.NotNil(t, res)
+
+		assert.Equal(t, res.ID, todo.ID)
+		assert.Equal(t, res.CreateAt, todo.CreatedAt)
+		assert.Equal(t, res.UpdateAt, todo.UpdatedAt)
+		assert.Equal(t, res.Topic, todo.Topic)
+		assert.Equal(t, res.Detail, todo.Detail)
+		assert.Equal(t, res.DueDate, todo.DueDate)
+		assert.Equal(t, res.IsDone, todo.IsDone)
+	})
+
+	t.Run("update fail, topic is blank", func(t *testing.T) {
+		db, _, repo := createMock()
+
+		content := &model.TodoContent{
+			Topic:   "",
+			Detail:  ptr.String("test detail"),
+			DueDate: ptr.Time(time.Now()),
+		}
+
+		u := NewTodoUseCase(db, &repo)
+
+		res, err := u.UpdateTodo(context.Background(), content)
+
+		require.Nil(t, res)
+		require.NotNil(t, err)
+		require.True(t, pkgerror.IsHttpError(err))
+
+		httpErr := err.(pkgerror.HttpError)
+		assert.Equal(t, httpErr.Code(), http.StatusBadRequest)
+		assert.Equal(t, httpErr.Error(), "topic is blank")
+	})
+
+	t.Run("update fail, database error", func(t *testing.T) {
+		db, smock, repo := createMock()
+
+		repo.On("UpdateTodo", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil, fmt.Errorf("db error"))
+
+		smock.ExpectBegin()
+		smock.ExpectRollback()
+
+		u := NewTodoUseCase(db, &repo)
+
+		res, err := u.UpdateTodo(context.Background(), &model.TodoContent{Topic: "test topic"})
+
+		require.Nil(t, res)
+		require.NotNil(t, err)
+
+		assert.Equal(t, err.Error(), "db error")
+	})
+}
